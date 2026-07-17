11fc4addd12db1f35629c617568bf690cbdb0ae4
AbdulrahmanNahhas <nahhas909@protonmail.com>
2026-07-17 11:28:18 +0300

    fix: dnscrypt-proxy

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,8 +1,9 @@
+# Encrypted DNS using dnscrypt-proxy.
 # See https://wiki.nixos.org/wiki/Encrypted_DNS
 
 let
   hasIPv6Internet = true;
-  StateDirectory = "dnscrypt-proxy";
+  stateDirectory = "dnscrypt-proxy";
 in
 {
   services.dnscrypt-proxy = {
@@ -10,20 +11,16 @@ in
 
     # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
     settings = {
-      listen_addresses = [
-        "127.0.0.1:53"
-        "[::1]:53"
-      ];
+      listen_addresses = [ "127.0.0.1:53" ];
 
       sources.public-resolvers = {
         urls = [
           "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-          "https://download.dnscrypt.info/dnscrypt-resolvers/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
         ];
 
-        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT1TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
-
-        cache_file = "/var/lib/${StateDirectory}/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        cache_file = "/var/lib/${stateDirectory}/public-resolvers.md";
       };
 
       # IPv4 / IPv6
@@ -38,11 +35,8 @@ in
 
       # Security requirements
       require_dnssec = true;
-      require_nolog = true;
+      require_nolog = false;
       require_nofilter = true;
-
-      # Ignore DHCP provided DNS
-      ignore_system_dns = true;
 
       # Prevent local hostname leaks
       block_unqualified = true;
@@ -60,5 +54,5 @@ in
     };
   };
 
-  systemd.services.dnscrypt-proxy.serviceConfig.StateDirectory = StateDirectory;
+  systemd.services.dnscrypt-proxy.serviceConfig.StateDirectory = stateDirectory;
 }
