0680e82cb1b406dfe051c92f429e409f82cd2881
AbdulrahmanNahhas <nahhas909@protonmail.com>
2026-08-01 16:33:14 +0300

    chore: Configure Noto fallback fonts and defaults

--- modules/nixos/desktop/fonts.nix
+++ modules/nixos/desktop/fonts.nix
@@ -1,4 +1,17 @@
 { pkgs, ... }:
 {
-  fonts.packages = with pkgs.nerd-fonts; [ geist-mono ];
+  fonts.packages = with pkgs; [
+    nerd-fonts.geist-mono
+
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+  ];
+
+  fonts.fontconfig.defaultFonts = {
+    monospace = [ "GeistMono Nerd Font" ];
+    sansSerif = [ "Noto Sans" ];
+    serif = [ "Noto Serif" ];
+    emoji = [ "Noto Color Emoji" ];
+  };
 }
