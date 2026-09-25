d5dd68e5b96ceffc5dd232e5be251940bf20defa
AbdulrahmanNahhas <nahhas909@protonmail.com>
2026-09-25 14:41:29 +0300

    zed: update keymaps

--- modules/home/programs/zed/keymaps.nix
+++ modules/home/programs/zed/keymaps.nix
@@ -3,22 +3,44 @@ _: {
     {
       context = "Workspace";
       bindings = {
-        # Individual panels and center views: Ctrl+Shift plus a mnemonic.
+        # Individual panels & docks
         "ctrl-shift-e" = "project_panel::ToggleFocus";
         "ctrl-shift-a" = "agent::ToggleFocus";
         "ctrl-shift-b" = "outline_panel::ToggleFocus";
         "ctrl-shift-c" = "collab_panel::ToggleFocus";
         "ctrl-shift-g" = "git_panel::ToggleFocus";
         "ctrl-shift-d" = "debug_panel::ToggleFocus";
-        "ctrl-shift-f" = "pane::DeploySearch";
+        "ctrl-shift-f" = "project_search::ToggleFocus"; # Replaced pane search with global search focus
         "ctrl-shift-m" = "diagnostics::Deploy";
 
-        # Whole docks and special sidebars.
+        # Dock and sidebar toggles
         "ctrl-b" = "workspace::ToggleLeftDock";
         "ctrl-alt-b" = "workspace::ToggleRightDock";
         "ctrl-j" = "workspace::ToggleBottomDock";
         "ctrl-`" = "terminal_panel::Toggle";
         "ctrl-alt-j" = "multi_workspace::ToggleWorkspaceSidebar";
+      };
+    }
+    {
+      context = "Pane";
+      bindings = {
+        # Linear sequential tab navigation (left-to-right order)
+        "ctrl-tab" = "pane::ActivateNextItem";
+        "ctrl-shift-tab" = "pane::ActivatePreviousItem";
+
+        # Tab management
+        "ctrl-w" = "pane::CloseActiveItem";
+        "ctrl-shift-t" = "pane::ReopenClosedItem";
+      };
+    }
+    {
+      context = "Editor";
+      bindings = {
+        # Line manipulation & editing enhancements
+        "alt-up" = "editor::MoveLineUp";
+        "alt-down" = "editor::MoveLineDown";
+        "shift-alt-down" = "editor::DuplicateLineDown";
+        "ctrl-/" = "editor::ToggleComments";
       };
     }
   ];
